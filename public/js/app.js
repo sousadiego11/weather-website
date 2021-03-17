37ed40c378f4354618d5aa812433e69e0d82c38b
Diego Sousa <diegointeriores11@gmail.com>
2021-03-17 20:11:01 -0300

    Sending project

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+const handleFetch = location => {
+    messageOne.textContent = 'Loading weather...'
+    messageTwo.textContent = ''
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(response => {
+        response.json().then(data => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                console.log(data)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+}
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    handleFetch(location)
+})
